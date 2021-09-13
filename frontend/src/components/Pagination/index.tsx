fc1c0e9e49b360a7f19925e65095fd98573a139f
SnoopyBR <juliopedroso@gmail.com>
2021-09-13 00:05:51 -0300

    Fixed pagination component position

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -11,7 +11,7 @@ const Pagination = ({page, onPageChange}: Props) => {
 
     return (
 
-        <div className="row d-flex justify-content-center">
+        <div className="d-flex justify-content-center">
             <nav>
                 <ul className="pagination">
                     <li className={`page-item ${page.first ? 'disabled': '' }` } >
